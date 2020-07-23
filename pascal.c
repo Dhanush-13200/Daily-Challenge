65549f4b0178814a8574b2dcde9df36ed9bc1b16
Dhanush-13200 <66692907+Dhanush-13200@users.noreply.github.com>
2020-07-23 15:46:33 +0530

    Update pascal.c

--- pascal.c
+++ pascal.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*
+Input :
+5
+Output :
+1 4 6 4 1
+
+*/
 
 int main()
 {
-    int x,i,j,num=1;
-    scanf("%d",&x);
+    long long int x,i,j,num=1;
+    scanf("%lld",&x);
     for(i=0 ; i<x ; i++)
     {
         for(j=0 ; j<=i ; j++)
@@ -19,7 +26,7 @@ int main()
             }
             if(i==x-1)
             {
-                printf("%d ",num);
+                printf("%lld ",num);
             }
         }
     }
